2812cc5a41874977afdef2a8d66caa701973374b
majidalykhan <majidalykhan@gmail.com>
2023-09-27 02:01:01 +0500

    Showing additional info

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,14 +1,27 @@
 import { React } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 
 const ResultsDetail = ({ result }) => {
   return (
     <View>
+      <Image style={styles.imageStyle} source={{ uri: result.image_url }} />
       <Text>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 250,
+    height: 100,
+    borderRadius: 4,
+  },
+  name: {
+    fontWeight: "bold",
+  },
+});
 
 export default ResultsDetail;
